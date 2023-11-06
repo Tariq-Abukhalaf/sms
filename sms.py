e1b5b90d936ff87c10fb87a3bb910e203bcd2c58
mr-tareq <tareqwaleed1996@gmail.com>
2023-11-06 10:52:46 +0300

    tareq

--- sms.py
+++ sms.py
@@ -1,3 +1,30 @@
-print('hi')
+import serial
+import re
+import time
 
-print('hi2')
+class SIM800L:
+    def __init__(self, serial_port, baud_rate):
+        self.serial = serial.Serial(serial_port, baud_rate, timeout=1)
+
+    def clear_serial(self):
+        self.serial.flushInput()
+        self.serial.flushOutput()
+
+    def signal_strength(self):
+        self.clear_serial()
+        self.serial.write(b'AT+CSQ\r\n')
+        time.sleep(1)  # Wait for the response
+        serial_buffer = self.serial.read(self.serial.in_waiting).decode('utf-8')
+
+        match = re.search(r'\+CSQ: (\d+),', serial_buffer)
+        if match:
+            signal_strength = int(match.group(1))
+            return signal_strength
+
+
+sim800 = SIM800L('/dev/serial0', 115000) 
+
+signal_strength = sim800.signal_strength()
+print(f'Signal Strength: {signal_strength}')
+
+sim800.serial.close()
