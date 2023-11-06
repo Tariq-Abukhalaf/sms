e6b7e9dc815afd4adee8459edc33f20f4b27396e
mr-tareq <tareqwaleed1996@gmail.com>
2023-11-06 17:07:47 +0300

    tareq

--- decorator.py
+++ decorator.py
@@ -9,7 +9,6 @@ def time_it(func):
         result = func(*args, **kwargs)
         end_time = time.time()
         elapsed_time = end_time - start_time
-        print(f"{func.__name__} took {elapsed_time:.6f} seconds to execute.")
         logging.info(f"{func.__name__} took {elapsed_time:.6f} seconds to execute.")
         return result
     return wrapper
