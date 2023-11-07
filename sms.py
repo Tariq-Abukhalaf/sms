cbd9e21284685f625761ac6e2cfb62185d354657
mr-tareq <tareqwaleed1996@gmail.com>
2023-11-07 15:53:51 +0300

    tareq

--- sms.py
+++ sms.py
@@ -247,8 +247,11 @@ class SIM800L:
             self.clear_serial()
             self.serial.write(f'AT+CMGL="ALL"\r\n'.encode())
             # time.sleep(10)
-            serial_buffer = self.read_serial()
+            # serial_buffer = self.read_serial()
+
+            serial_buffer = self.serial.read(self.serial.in_waiting).decode('utf-8')
             print(serial_buffer)
+
             indices = re.findall(r'\+CMGL: (\d+),', serial_buffer)
             return indices
         return -1
