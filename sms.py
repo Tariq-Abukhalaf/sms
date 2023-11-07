e695db74b727ebf24c2ebbf4b08ef954ba7dbf22
mr-tareq <tareqwaleed1996@gmail.com>
2023-11-07 10:02:14 +0300

    tareq

--- sms.py
+++ sms.py
@@ -196,7 +196,7 @@ class SIM800L:
             if 'OK' in serial_buffer:
                 self.clear_serial()
                 self.serial.write(b'AT+CMGL="ALL"\r\n')
-                serial_buffer = self.read_serial_timeout(10000)
+                serial_buffer = self.read_serial_timeout(20000)
                 print(serial_buffer)
 
 
