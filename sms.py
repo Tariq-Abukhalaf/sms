eaab6ff7d15803d15e733d6d75005df0cf860f5d
mr-tareq <tareqwaleed1996@gmail.com>
2023-11-07 17:10:55 +0300

    tareq

--- sms.py
+++ sms.py
@@ -35,7 +35,7 @@ class SIM800L:
         start_time = time.time()
         prev_waiting_byte = 0 
         
-        while (self.serial.in_waiting == prev_waiting_byte):
+        while (self.serial.in_waiting != prev_waiting_byte):
             print(time.time() - start_time,end='\n')
             print(self.serial.in_waiting,end='\n')
             time.sleep(1)
