03542939e8d5546856fc1da0b3b7ecf3efce17c2
mr-tareq <tareqwaleed1996@gmail.com>
2023-11-08 15:45:11 +0300

    tareq

--- sms.py
+++ sms.py
@@ -294,11 +294,10 @@ class SIM800L:
         response = self.read_serial(b'>')
         if '>' in response:
             self.clear_serial()
-            self.serial.write(message.encode() + bytes([26]))
+            self.serial.write(message + bytes([26]))
             self.set_timeout(3)
             response = self.read_serial(b'OK\r\n')
             self.set_timeout(0.1)
-            print(response)
             if "OK" in response:
                 return True
             return False
