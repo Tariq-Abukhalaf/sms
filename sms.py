e41ac42bd1baf30e28dbd35622c57b77ffd3a715
mr-tareq <tareqwaleed1996@gmail.com>
2023-11-08 13:35:30 +0300

    tareq

--- sms.py
+++ sms.py
@@ -261,8 +261,7 @@ class SIM800L:
         if (sim800.set_text_mode(0)):
             self.clear_serial()
             self.serial.write(f'AT+CUSD=1,"{ussd_code}",15'.encode())
-            time.sleep(10)
-            serial_buffer = self.serial.read().decode('utf-8') 
+            serial_buffer = self.serial.read_serial(self, b'\r\n>')
             print(serial_buffer)
             if 'OK' in serial_buffer:
                 # serial_buffer  = serial_buffer.replace('+CUSD: 0, ','').replace('"','').replace('OK', '')
