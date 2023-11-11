e695da42cda398056232081a34a949d006b6bd71
mr-tareq <tareqwaleed1996@gmail.com>
2023-11-11 18:10:17 +0300

    tareq

--- test.py
+++ test.py
@@ -4,7 +4,7 @@ def get_available_network_ssids():
     try:
         result = subprocess.run(['sudo', 'iwlist', 'wlan0', 'scan'], capture_output=True, text=True, check=True)
         if result.returncode == 0:
-            network_info = result.stdout.split('ESSID:')
+            network_info = result.stdout.split('ESSID:')[1:]
             print(network_info)
             return
             ssids = [line.split('\n')[0].strip().strip('"') for line in network_info]
