fb77978c5145272dcd59e9c9a4c55ef493dc399d
mr-tareq <tareqwaleed1996@gmail.com>
2023-11-11 18:02:25 +0300

    tareq

--- test.py
+++ test.py
@@ -1,3 +1,5 @@
+import subprocess
+
 def get_available_networks():
     result = subprocess.run(['sudo', 'iwlist', 'wlan0', 'scan'], capture_output=True, text=True)
     networks = [line.strip() for line in result.stdout.split('ESSID:')[1:]]
