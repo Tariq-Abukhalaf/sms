514dff5109af436fc52117542fd4884859d9cbdb
mr-tareq <tareqwaleed1996@gmail.com>
2023-11-11 23:38:43 +0300

    tareq

--- app.py
+++ app.py
@@ -45,21 +45,28 @@ def parse_wifi_list():
         print(f"Error running 'nmcli': {e}")
         return []
 
+# def get_available_network_ssids():
+#     try:
+#         result = subprocess.run(['sudo', 'iwlist', 'wlan0', 'scan'], capture_output=True, text=True, check=True)
+#         if result.returncode == 0:
+#             network_info = result.stdout.split('ESSID:')[1:]
+#             ssids = [line.split('\n')[0].strip().strip('"') for line in network_info]
+#             return ssids
+#         else:
+#             print("Error: 'iwlist' command returned a non-zero exit code.")
+#             return []
+#     except Exception as e:
+#         print(f"An error occurred: {str(e)}")
+#         return []
+
 def get_available_network_ssids():
-    try:
-        result = subprocess.run(['sudo', 'iwlist', 'wlan0', 'scan'], capture_output=True, text=True, check=True)
-        if result.returncode == 0:
-            network_info = result.stdout.split('ESSID:')[1:]
-            ssids = [line.split('\n')[0].strip().strip('"') for line in network_info]
-            return ssids
-        else:
-            print("Error: 'iwlist' command returned a non-zero exit code.")
-            return []
-    except Exception as e:
-        print(f"An error occurred: {str(e)}")
-        return []
-
-
+    nts = []
+    wifi_list = parse_wifi_list()
+    for item in wifi_list:
+        for key, value in item.items():
+            if key == 'SSID':
+                nts.append(value)
+    return nts
 
 
 def get_nm_connections():
