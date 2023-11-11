9ce6595a08d8c31865f621b0c248e825448c4a84
mr-tareq <tareqwaleed1996@gmail.com>
2023-11-11 15:32:48 +0300

    tareq

--- sms.py
+++ sms.py
@@ -360,13 +360,13 @@ class SIM800L:
 
             udh = f"{total_parts:02X}{part_number + 1:02X}"
 
-            self.ser.write(b'AT+CMGF=0\r\n') 
-            self.ser.write(f'AT+CMGS={len(part_message) + 7}\r\n'.encode())
+            self.serial.write(b'AT+CMGF=0\r\n') 
+            self.serial.write(f'AT+CMGS={len(part_message) + 7}\r\n'.encode())
             time.sleep(1)
-            self.ser.write(f'{udh}{recipient_number.encode()}00'.encode())
-            self.ser.write(part_message.encode() + bytes([26]))
+            self.serial.write(f'{udh}{recipient_number.encode()}00'.encode())
+            self.serial.write(part_message.encode() + bytes([26]))
             time.sleep(2)
-            response = self.ser.read(1000)
+            response = self.read_serial()
             print(response)
 
 
@@ -432,7 +432,7 @@ if len(list_sms_indices) != 0:
 # print('**************************************',end='\n')
 
 
-# sim800.send_concatenated_sms('0789221769', "There are approximately 60,000 hairs per square inch on the back of a cat and about 120,000 per square inch on its underside.120,000 hi from other side hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh")
+sim800.send_concatenated_sms('0789221769', "There are approximately 60,000 hairs per square inch on the back of a cat and about 120,000 per square inch on its underside.120,000 hi from other side hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh")
 sim800.close()
 
 
