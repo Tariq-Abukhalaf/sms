73395abfb5418764e17b93e5b27dd42d0ef63e02
mr-tareq <tareqwaleed1996@gmail.com>
2023-11-11 18:15:06 +0300

    tareq

--- app.py
+++ app.py
@@ -5,11 +5,19 @@ import subprocess
 
 app = Flask(__name__)
 
-def get_available_networks():
-    result = subprocess.run(['sudo', 'iwlist', 'wlan0', 'scan'], capture_output=True, text=True)
-    networks = [line.strip() for line in result.stdout.split('ESSID:')[1:]]
-    print(networks)
-    return networks
+def get_available_network_ssids():
+    try:
+        result = subprocess.run(['sudo', 'iwlist', 'wlan0', 'scan'], capture_output=True, text=True, check=True)
+        if result.returncode == 0:
+            network_info = result.stdout.split('ESSID:')[1:]
+            ssids = [line.split('\n')[0].strip().strip('"') for line in network_info]
+            return ssids
+        else:
+            print("Error: 'iwlist' command returned a non-zero exit code.")
+            return []
+    except Exception as e:
+        print(f"An error occurred: {str(e)}")
+        return []
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -18,7 +26,7 @@ def index():
         password = request.form['password']
         with open('/etc/wpa_supplicant/wpa_supplicant.conf', 'a') as wpa_conf:
             wpa_conf.write(f'network={{\n  ssid="{selected_network}"\n  psk="{password}"\n}}\n')
-    networks = get_available_networks()
+    networks = get_available_network_ssids()
     return render_template('index.html', networks=networks)
 
 if __name__ == '__main__':
