0d84d46da4ab61b55da47c485004f2d71b5a5237
mr-tareq <tareqwaleed1996@gmail.com>
2023-11-11 18:06:54 +0300

    tareq

--- test.py
+++ test.py
@@ -1,10 +1,19 @@
 import subprocess
 
-def get_available_networks():
-    result = subprocess.run(['sudo', 'iwlist', 'wlan0', 'scan'], capture_output=True, text=True)
-    networks = [line.strip() for line in result.stdout.split('ESSID:')[1:]]
-    print(networks)
-    return networks
-
-
-get_available_networks()
+def get_available_network_ssids():
+    try:
+        result = subprocess.run(['sudo', 'iwlist', 'wlan0', 'scan'], capture_output=True, text=True, check=True)
+        print(result)
+        return 
+        if result.returncode == 0:
+            network_info = result.stdout.split('ESSID:')[1:]
+            ssids = [line.split('\n')[0].strip().strip('"') for line in network_info]
+            return ssids
+        else:
+            print("Error: 'iwlist' command returned a non-zero exit code.")
+            return []
+    except Exception as e:
+        print(f"An error occurred: {str(e)}")
+        return []
+    
+get_available_network_ssids()
