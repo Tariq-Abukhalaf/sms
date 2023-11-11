6e75d0a993546b240faa8ae5fe0b972ffd660ce5
mr-tareq <tareqwaleed1996@gmail.com>
2023-11-11 17:43:53 +0300

    tareq

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
+#!/usr/bin/python3
+
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def index():
+        return 'Hello world'
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=5050)
+        app.run(debug=True, host='0.0.0.0', port=4000)
+
 
