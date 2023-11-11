2608059952324f69b2dcffb33dba14e93ad8b09d
mr-tareq <tareqwaleed1996@gmail.com>
2023-11-11 20:27:32 +0300

    tareq

--- test.py
+++ test.py
@@ -3,17 +3,10 @@ import re
 
 def parse_wifi_networks():
     output = subprocess.check_output(['nmcli', 'device', 'wifi', 'list'], text=True)
-    pattern = r'(?P<SSID>.+?)\s+(?P<BSSID>\S+)\s+(?P<MODE>\S+)\s+(?P<FREQ>\S+)\s+(?P<RATE>\S+)\s+(?P<SIGNAL>\S+)\s+(?P<SECURITY>.+)$'
+    pattern = r'(?P<INUSE>.+?)\s+(?P<BSSID>.+?)\s+(?P<SSID>\S+)\s+(?P<MODE>\S+)\s+(?P<CHAN>.+?)\s+(?P<RATE>\S+)\s+(?P<SIGNAL>\S+)\s+(?P<BARS>\S+)\s+(?P<SECURITY>.+)$'
     wifi_networks = []
     for match in re.finditer(pattern, output, re.MULTILINE):
-        network_info = match.groupdict()
-        # Create a new dictionary with only BSSID, SSID, and SIGNAL
-        simplified_network_info = {
-            'BSSID': network_info['BSSID'],
-            'SSID': network_info['SSID'],
-            'SIGNAL': network_info['SIGNAL']
-        }
-        wifi_networks.append(simplified_network_info)
+        wifi_networks.append(match.groupdict())
     return wifi_networks
     
 print(parse_wifi_networks())
