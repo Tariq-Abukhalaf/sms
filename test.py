29031b366af9000c5a3d6ecdb64a8703d73cfe52
mr-tareq <tareqwaleed1996@gmail.com>
2023-11-11 20:18:19 +0300

    tareq

--- test.py
+++ test.py
@@ -6,7 +6,14 @@ def parse_wifi_networks():
     pattern = r'(?P<SSID>.+?)\s+(?P<BSSID>\S+)\s+(?P<MODE>\S+)\s+(?P<FREQ>\S+)\s+(?P<RATE>\S+)\s+(?P<SIGNAL>\S+)\s+(?P<SECURITY>.+)$'
     wifi_networks = []
     for match in re.finditer(pattern, output, re.MULTILINE):
-        wifi_networks.append(match.groupdict())
+        network_info = match.groupdict()
+        # Create a new dictionary with only BSSID, SSID, and SIGNAL
+        simplified_network_info = {
+            'BSSID': network_info['BSSID'],
+            'SSID': network_info['SSID'],
+            'SIGNAL': network_info['SIGNAL']
+        }
+        wifi_networks.append(simplified_network_info)
     return wifi_networks
     
 print(parse_wifi_networks())
