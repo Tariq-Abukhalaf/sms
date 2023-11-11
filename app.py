5f100ae4539a1b09a1834e10a62908a860e10f31
mr-tareq <tareqwaleed1996@gmail.com>
2023-11-11 18:00:37 +0300

    tareq

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ app = Flask(__name__)
 def get_available_networks():
     result = subprocess.run(['sudo', 'iwlist', 'wlan0', 'scan'], capture_output=True, text=True)
     networks = [line.strip() for line in result.stdout.split('ESSID:')[1:]]
+    print(networks)
     return networks
 
 @app.route('/', methods=['GET', 'POST'])
